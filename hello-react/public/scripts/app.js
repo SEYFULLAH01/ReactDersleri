bc38d9e97b3e5522ca0623b8314bbba0600e4873
SEYFULLAH01 <120310366+SEYFULLAH01@users.noreply.github.com>
2024-05-16 01:17:18 +0300

    1
    Genl bakış ve babel kurulumu yapıldı. ReactJSX gelecek derste bakılacak

---
+++ hello-react/public/scripts/app.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var root = document.getElementById('root');
+var template = /*#__PURE__*/React.createElement("h1", {
+  id: "header"
+}, "Hello React");
+ReactDOM.render(template, root);
---
+++ hello-react/src/app.js
@@ -0,0 +1,7 @@
+
+
+    var root = document.getElementById('root');
+    var template = <h1 id="header">Hello React</h1>
+
+    ReactDOM.render(template, root)
+
